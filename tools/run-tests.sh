7017374e4dc3356428d1685d5a0faf995f971f43
chengxiang li <chengxiang.li@intel.com>
2015-05-29 16:03:31 +0800

    add maven option

--- tools/run-tests.sh
+++ tools/run-tests.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-
+export MAVEN_OPTS="-Xms1536m -Xmx1536m -XX:PermSize=1024m -XX:MaxPermSize=2048m"
 mvn clean install -DskipTests -Phadoop-2
 cd itests
 mvn clean test -DTestSparkCliDriver -Phadoop-2
